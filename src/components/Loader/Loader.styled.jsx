1057f9e3e792878c9437be346a0843343ea63403
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-06-08 11:28:43 +0200

    add initial structure and styles

---
+++ src/components/Loader/Loader.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const LoaderWrap = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
